a3611d44069c8c729dbbb191ec152919c5d8a9d3
Julian Neytchev <dobriak@gmail.com>
2017-10-31 00:36:48 -0700

    Escaping weirdness

--- sample_apps.sh
+++ sample_apps.sh
@@ -16,16 +16,16 @@ function start(){
 
     echo "Creating sample application monitors"
     #cpu
-    curl -X POST -d '{ "app_id": "\/${NAMESPACE}/test1","max_cpu": 60,"min_cpu": 20,"max_mem": 90,"min_mem": 5,"method": "cpu","scale_factor": 1,"max_instances": 6,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 19}' ${AS_URL}/apps
+    curl -X POST -d '{ "app_id": "/'${NAMESPACE}'/test1","max_cpu": 60,"min_cpu": 20,"max_mem": 90,"min_mem": 5,"method": "cpu","scale_factor": 1,"max_instances": 6,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 19}' ${AS_URL}/apps
     sleep 5s
     #mem
-    curl -X POST -d '{"app_id": "\/${NAMESPACE}/test2","max_cpu": 60,"min_cpu": 20,"max_mem": 75,"min_mem": 15,"method": "mem","scale_factor": 1,"max_instances": 5,"min_instances": 2,"warm_up": 3,"cool_down": 4,"interval": 21}' ${AS_URL}/apps
+    curl -X POST -d '{"app_id": "/'${NAMESPACE}'/test2","max_cpu": 60,"min_cpu": 20,"max_mem": 75,"min_mem": 15,"method": "mem","scale_factor": 1,"max_instances": 5,"min_instances": 2,"warm_up": 3,"cool_down": 4,"interval": 21}' ${AS_URL}/apps
     sleep 5s
     #and
-    curl -X POST -d '{"app_id": "\/${NAMESPACE}/test3","max_cpu": 55,"min_cpu": 15,"max_mem": 80,"min_mem": 10,"method": "and","scale_factor": 1,"max_instances": 6,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 23}' ${AS_URL}/apps
+    curl -X POST -d '{"app_id": "/'${NAMESPACE}'/test3","max_cpu": 55,"min_cpu": 15,"max_mem": 80,"min_mem": 10,"method": "and","scale_factor": 1,"max_instances": 6,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 23}' ${AS_URL}/apps
     sleep 5s
     #or
-    curl -X POST -d '{"app_id": "\/${NAMESPACE}/test4","max_cpu": 70,"min_cpu": 50,"max_mem": 75,"min_mem": 50,"method": "or","scale_factor": 1,"max_instances": 5,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 25}' ${AS_URL}/apps
+    curl -X POST -d '{"app_id": "/'${NAMESPACE}'/test4","max_cpu": 70,"min_cpu": 50,"max_mem": 75,"min_mem": 50,"method": "or","scale_factor": 1,"max_instances": 5,"min_instances": 2,"warm_up": 3,"cool_down": 3,"interval": 25}' ${AS_URL}/apps
 
 }
 
